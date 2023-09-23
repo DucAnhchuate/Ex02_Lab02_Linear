6f9a5dc0d28d73bf775d859519277a4234ee905a
DucAnh <521h0005@student.tdtu.edu.vn>
2023-09-23 16:59:50 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ex02_Lab02_Linear"
+include(":app")
+ 
